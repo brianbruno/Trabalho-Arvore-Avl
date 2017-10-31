06d1005b2fe0878d78fd3074f61b4bd4e6a70a00
Lucas Silveira Macedo <1078086@sga.pucminas.br>
2017-10-31 11:27:41 -0200

    Nova impressao no console

--- src/Principal/Aplicacao.java
+++ src/Principal/Aplicacao.java
@@ -3,5 +3,6 @@ package Principal;
 public class Aplicacao {
     public static void main(String[] args) {
         System.out.println("Trabalho de laboratório.");
+        System.out.println("First");
     }
 }
